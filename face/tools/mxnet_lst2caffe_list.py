ab3c741d945d9ba74bfa5f69487ff6cdea019eb0
zjwzcnjsy <876094812@qq.com>
2017-11-13 12:49:44 +0800

    fix bug

--- face/tools/mxnet_lst2caffe_list.py
+++ face/tools/mxnet_lst2caffe_list.py
@@ -1,12 +1,13 @@
 import os
 import argparse
 
+
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--infile', required=True, type=str)
     parser.add_argument('--outfile', required=True, type=str)
-
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse()
@@ -14,7 +15,6 @@ if __name__ == '__main__':
     with open(args.infile, mode='r') as fin:
         with open(args.outfile, mode='w') as fout:
             for line in fin:
-               if line.strip():
-                   items = line.strip().split()
-                   fout.write('{} {}\n'.format(items[2], items[1])
-
+                if line.strip():
+                    items = line.strip().split()
+                    fout.write('{} {}\n'.format(items[2], items[1])
