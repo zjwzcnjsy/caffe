f5df999ea48b0b044e06fd7da3f4a61f235742ce
zjwzcnjsy <876094812@qq.com>
2017-11-14 10:19:11 +0800

    添加sphereface64

---
+++ face/sphereface/sphereface64/code/sphereface_train.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Usage:
+# ./code/sphereface_train.sh GPU
+#
+# Example:
+# ./code/sphereface_train.sh 0,1,2,3
+GPU_ID=$1
+$CAFFE_HOME/build/tools/caffe train -solver code/sphereface_solver.prototxt -gpu ${GPU_ID} 2>&1 | tee result/sphereface_train.log
