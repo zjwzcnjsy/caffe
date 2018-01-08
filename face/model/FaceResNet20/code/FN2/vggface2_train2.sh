e16d194a6c220f9890f605a24d56da1d0a1b3310
zjwzcnjsy <876094812@qq.com>
2018-01-08 20:11:08 +0800

    feat: face相关实验移到新的repo

--- face/model/FaceResNet20/code/FN2/vggface2_train2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Usage:
-# ./code/train.sh GPU
-#
-# Example:
-# ./code/train.sh 0,1,2,3
-GPU_ID=$1
-$CAFFE_HOME/build/tools/caffe train -solver code/FN2/FaceResNet20_vggface2_step4_solver.prototxt -weights result/FN2/FaceResNet20_vggface2_step3_iter_60000.caffemodel -gpu ${GPU_ID}
-$CAFFE_HOME/build/tools/caffe train -solver code/FN2/FaceResNet20_vggface2_step5_solver.prototxt -weights result/FN2/FaceResNet20_vggface2_step4_iter_60000.caffemodel -gpu ${GPU_ID}
--- face/model/sphereface/sphereface64/code/FN2/train2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Usage:
-# ./code/train.sh GPU
-#
-# Example:
-# ./code/train.sh 0,1,2,3
-GPU_ID=$1
-$CAFFE_HOME/build/tools/caffe train -solver code/FN2/sphereface64_vggface2_step4_solver.prototxt -weights result/FN2/sphereface64_vggface2_step3_iter_60000.caffemodel -gpu ${GPU_ID}
